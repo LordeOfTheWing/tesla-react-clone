b7847c4603b90712bd42430d4fc93321cd5ea305
Clinton Mainya <klyntonmainya@gmail.com>
2022-03-23 15:38:07 +0300

    Add props to the Section component

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,9 +12,47 @@ const Home = () => {
         leftBtnText='Custom Order'
         rightBtnText='Existing Inventory'
       />
-      {/* <Section /> */}
-      {/* <Section /> */}
-      {/* <Section /> */}
+      <Section
+        title='Model Y'
+        description='Order Online for Touchless Delivery'
+        backgroundImg='model-y.jpg'
+        leftBtnText='Custom Order'
+        rightBtnText='Existing Inventory'
+      />
+      <Section
+        title='Model 3'
+        description='Order Online for Touchless Delivery'
+        backgroundImg='model-3.jpg'
+        leftBtnText='Custom Order'
+        rightBtnText='Existing Inventory'
+      />
+      <Section
+        title='Model X'
+        description='Order Online for Touchless Delivery'
+        backgroundImg='model-x.jpg'
+        leftBtnText='Custom Order'
+        rightBtnText='Existing Inventory'
+      />
+      <Section
+        title='Lowest Cost Solar Panels In America'
+        description='Money-back guarantee'
+        backgroundImg='solar-panel.jpg'
+        leftBtnText='Order now'
+        rightBtnText='Learn more'
+      />
+      <Section
+        title='Solar for New Roofs'
+        description='Solar Roof Costs Less Than a New Roof Plus Solar Panels'
+        backgroundImg='solar-roof.jpg'
+        leftBtnText='Order now'
+        rightBtnText='Learn more'
+      />
+      <Section
+        title='Accessories'
+        description=''
+        backgroundImg='accessories.jpg'
+        leftBtnText='Shop now'
+      />
     </Container>
   );
 };
